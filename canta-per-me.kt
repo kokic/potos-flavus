6b69f097a86eb488baf70537c52862f6391d30a4
kokic <3250679033@qq.com>
2022-11-10 01:14:16 +0800

    Add files via upload

---
+++ canta-per-me.kt
@@ -0,0 +1,44 @@
+
+fun main() {
+
+    canta per me ne addio
+    quel dolce suono
+    `de'` passati giorni
+    mi sempre rammenta
+
+    la vita `dell'amore`
+    dilette del cor mio                     ` `
+    o felice`,` tu anima mia                ` `
+    canta - addagio ..                      ` `
+
+    tempra la cetra e canta
+    il inno di morte                        ` `
+    a noi si schiude il ciel                ` `
+    volano al raggio
+
+    la vita `dell'amore`
+    dilette del cor mio                     ` `
+    o felice`,` tu anima mia                ` `
+    canta - addio ..                        ` `
+
+    la vita `dell'amore`
+    o - dilette del cor mio                 ` `
+
+}
+
+object canta: 𝜞() infix fun ℓ.per(a: ℓ) = a object me infix fun Any.ne(a: Any) = a object addio
+object quel infix fun ℓ.dolce(a: ℓ) = a object suono
+object `de'` infix fun ℓ.passati(a: ℓ) = a object giorni
+object mi infix fun ℓ.sempre(a: ℓ) = a object rammenta
+
+object la infix fun ℓ.vita(a: ℓ) = a object `dell'amore`
+object dilette infix fun ℓ.del(a: ℓ) = a object cor infix fun ℓ.mio(a: ℓ) = a
+infix fun ℓ.felice(a: ℓ) = a object o: 𝜞() infix fun ℓ.tu(a: ℓ) = a object anima infix fun ℓ.mia(a: ℓ) = a
+infix operator fun 𝜞.minus(a: ℓ) = canta object addagio infix operator fun 𝜞.rangeTo(a: ℓ) = a
+
+object tempra infix fun ℓ.la(a: ℓ) = a object cetra infix fun ℓ.e(a: ℓ) = a
+object il infix fun ℓ.inno(a: ℓ) = a object di infix fun ℓ.morte(a: ℓ) = a
+infix fun ℓ.noi(a: ℓ) = a object a infix fun ℓ.schiude(a: ℓ) = a object si infix fun ℓ.ciel(a: ℓ) = a
+object volano infix fun ℓ.al(a: ℓ) = a object raggio
+
+object ` ` object `,`  typealias ℓ = Any open class 𝜞
